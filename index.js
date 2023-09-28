19c8548ab88431e30b2abbb4427c0532a9cb9f45
Eckra Wangui <+>
2023-09-28 14:10:51 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+
+
+
+const mainElement = document.querySelector('main');
+  mainElement.remove();
+
+
+const newHeader = document.createElement('h1')
+newHeader.id= "victory"
+newHeader.innerHTML="Eckra is the champion"
+ newHeader.append();
+
